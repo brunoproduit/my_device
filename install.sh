a8d608a882253adbd028dc13636cd789a997da4d
Bruno Produit <bruno.produit@protonmail.ch>
2017-11-06 10:00:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ make
 
 sudo insmod my_device.ko
 sudo mknod /dev/my_device c 42 0
-sudo chmod 666 /dev/my_device*
+sudo chmod 666 /dev/my_device
 
 gcc -o device_usage_example device_usage_example.c
 ./device_usage_example
